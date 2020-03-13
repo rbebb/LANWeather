5511aca8788fc25670eb3442403b1ae95e864693
Ryan Bebb <bebbr@wit.edu>
2020-03-13 17:05:32 -0400

    Beginning to configure Android notifications

--- androidapp/app/src/main/java/com/example/lanweather/MainActivity.kt
+++ androidapp/app/src/main/java/com/example/lanweather/MainActivity.kt
@@ -1,7 +1,15 @@
 package com.example.lanweather
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.navigation.findNavController;
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
@@ -9,6 +17,9 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
+    private val CHANNEL_ID = "lan_weather_notifications"
+    private var notificationId = 1
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +33,42 @@ class MainActivity : AppCompatActivity() {
             R.id.navigation_home, R.id.navigation_forecast, R.id.navigation_settings))
 //        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        // Only used to test notifications
+//        val button: Button = findViewById(R.id.test_notification)
+//        button.setOnClickListener { displayNotification() }
+    }
+
+    private fun displayNotification() {
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_cloud_black_24dp)
+            .setContentTitle(getString(R.string.clothing))
+            .setContentText(String.format(getString(R.string.wear_this_insert),
+                getString(R.string.jacket)))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        createNotificationChannel()
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(notificationId, builder.build())
+        }
+        notificationId++
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
