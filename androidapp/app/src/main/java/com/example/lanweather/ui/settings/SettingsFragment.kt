e914474868d7300ad7bbe563147ef3e4d02dac27
Ryan Bebb <bebbr@wit.edu>
2020-03-13 11:58:04 -0400

    Working on card layouts and preferences

--- androidapp/app/src/main/java/com/example/lanweather/ui/settings/SettingsFragment.kt
+++ androidapp/app/src/main/java/com/example/lanweather/ui/settings/SettingsFragment.kt
@@ -6,28 +6,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
 
 import com.example.lanweather.R
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
-    companion object {
-        fun newInstance() = SettingsFragment()
+//    private lateinit var viewModel: SettingsViewModel
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
     }
 
-    private lateinit var viewModel: SettingsViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.settings_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(SettingsViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        return inflater.inflate(R.layout.settings_fragment, container, false)
+//    }
+//
+//    override fun onActivityCreated(savedInstanceState: Bundle?) {
+//        super.onActivityCreated(savedInstanceState)
+//        viewModel = ViewModelProviders.of(this).get(SettingsViewModel::class.java)
+//        // TODO: Use the ViewModel
+//    }
 }
