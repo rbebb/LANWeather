e44ba3cc99361cbc83429adeda19f60dea643b87
Tom Castle <minifigone@gmail.com>
2020-03-17 19:35:14 -0400

    [#20][#24] restructured things. some additions to the singleton header. some comments on rust.

--- lanweatherd/src/lib.rs
+++ lanweatherd/src/lib.rs
@@ -1,10 +1,10 @@
 use std::ffi::CString;
 use std::os::raw::c_char;
 
-#[no_mangle]
-pub extern "C" fn nws_req() -> *const c_char {
-    let s = CString::new("succeed").unwrap();
-    let p = s.as_ptr();
-    std::mem::forget(s);
-    p
+#[no_mangle] // don't mangle the name
+pub extern "C" fn nws_req() -> *const c_char { // returns a pointer to a c char array
+    let s = CString::new("succeed").unwrap(); // make a CString
+    let p = s.as_ptr(); // get a pointer to the CString
+    std::mem::forget(s); // don't have rust free the memory
+    p // return the pointer
 }
--- lanweatherd/src/recent_data.h
+++ lanweatherd/src/recent_data.h
@@ -4,10 +4,18 @@
  * http://www.modernescpp.com/index.php/thread-safe-initialization-of-a-singleton
 **/
 
+#pragma once
+#include <string>
+
 class recent_data {
     public:
         static recent_data& get_instance();
+        void update_nws_data(char* new_data);
+        void update_sensor_data(char* new_data);
+        char* get_data_bundle();
 
     private:
         recent_data();
+        char* nws_data;
+        char* sensor_data;
 };
