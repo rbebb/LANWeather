56fb313f5765e5ba0d03dcd276478ad24f713621
Tom Castle <minifigone@gmail.com>
2020-04-12 22:20:08 -0400

    [#25 ish] idfk. everything is breaking. my pi won't run anything. this daemon seems fine but also not.

--- lanweatherd/src/sensor_loop.cpp
+++ lanweatherd/src/sensor_loop.cpp
@@ -1,19 +1,24 @@
 #include <unistd.h>
 #include <zmq.h>
+#include <stdlib.h>
+#include <syslog.h>
 
 #include "sensor_loop.h"
 
 void sensor_loop(recent_data& cache){
     void *context = zmq_ctx_new();
-    void *responder = zmq_socket(context, ZMQ_REP);
-    zmq_bind(responder, "tcp://*:5675");
+    void *sock = zmq_socket(context, ZMQ_SUB);
+    zmq_bind(sock, "tcp://*:5676");
+    zmq_setsockopt(sock, ZMQ_SUBSCRIBE, "", 0);
 
-    while(1){
-        char buffer[64];
-        zmq_recv(responder, buffer, 63, 0);
+    while(1) {
+        char* buffer = (char*) malloc(64 * sizeof(char));
+        zmq_recv(sock, buffer, 63, 0);
+        syslog(LOG_NOTICE, "|%s|", buffer);
         cache.update_sensor_data(buffer);
+        free(buffer);
     }
 
-    zmq_close(responder);
+    zmq_close(sock);
     zmq_ctx_destroy(context);
 }
