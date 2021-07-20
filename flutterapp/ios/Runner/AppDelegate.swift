dfce2f5c4e74b581549a64f52e9767988b62bcb0
Ryan Bebb <bebbr@wit.edu>
2021-07-20 00:08:24 -0400

    Fix most iOS errors

--- flutterapp/ios/Runner/AppDelegate.swift
+++ flutterapp/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import SwiftyZeroMQ5
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,28 +9,44 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
 
-    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-    let channelFetchAllWeatherData = "com.lanweather.flutterapp/fetchallweatherdata"
-    let channel = FlutterMethodChannel(name: channelFetchAllWeatherData, binaryMessager: controller.binaryMessager)
+        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        let channelFetchAllWeatherData = "com.lanweather.flutterapp/fetchallweatherdata"
+        let channel = FlutterMethodChannel(name: channelFetchAllWeatherData, binaryMessenger: controller.binaryMessenger)
 
-    channel.setMethodCallHandler({
-        [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-        guard call.method == "fetchAllWeatherData" else {
-            result(FlutterMethodNotImplemented)
-            return
-        }
-        let url = call.argument["url"] as? String
-        let data = call.argument["data"] as? String
-        self?.fetchCurrentWeatherData(jsonData: call.)
-    })
+        channel.setMethodCallHandler({
+            [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+            guard call.method == "fetchAllWeatherData" else {
+                result(FlutterMethodNotImplemented)
+                return
+            }
+            let arguments = call.arguments as! Dictionary<String, String>
+            let url = arguments["url"]
+            let data = arguments["data"]
+            
+            var jsonData: String?
+            do {
+                let context = try SwiftyZeroMQ.Context()
+                let socket = try context.socket(SwiftyZeroMQ.SocketType.request)
+                try socket.connect(url!)
+                try socket.send(string: data!)
+                jsonData = try socket.recv()
+                try socket.close()
+            } catch {
+                print(error)
+            }
+            
+            let jsonDataObj = self?.getCurrentWeatherData(jsonData: jsonData!)
+            result(jsonDataObj)
+        })
 
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
     
-    private func fetchCurrentWeatherData(jsonData: String) -> String {
+    private func getCurrentWeatherData(jsonData: String) -> String {
         // Get nested JSON object
-        let jsonDataObj = JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any]
-        return String(jsonDataObj["nws"]["current"])
+//        let jsonDataObj = JSONSerialization.jsonObject(with: jsonData, options: JSONSerialization.ReadingOptions.mutableContainers) as? [String: Any]
+//        return String(jsonDataObj["nws"]["current"])
+        return ""
     }
 }
