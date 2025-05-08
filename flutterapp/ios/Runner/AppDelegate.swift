f739946d94a2059851f4a31a3cca3cd47df9316a
rbebb <bebbr@wit.edu>
2025-05-08 00:40:06 -0400

    Temporarily disable ZMQ requests for iOS

--- flutterapp/ios/Runner/AppDelegate.swift
+++ flutterapp/ios/Runner/AppDelegate.swift
@@ -2,7 +2,7 @@ import UIKit
 import Flutter
 import SwiftyZeroMQ5
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
     override func application(
     _ application: UIApplication,
@@ -13,51 +13,45 @@ import SwiftyZeroMQ5
         let channelFetchAllWeatherData = "com.lanweather.flutterapp/fetchallweatherdata"
         let channel = FlutterMethodChannel(name: channelFetchAllWeatherData, binaryMessenger: controller.binaryMessenger)
 
-        channel.setMethodCallHandler({
-            [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-            guard call.method == "fetchAllWeatherData" else {
-                result(FlutterMethodNotImplemented)
-                return
-            }
-            let arguments = call.arguments as! Dictionary<String, String>
-            let url = arguments["url"]
-            let data = arguments["data"]
+        // channel.setMethodCallHandler({
+        //     [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+        //     guard call.method == "fetchAllWeatherData" else {
+        //         result(FlutterMethodNotImplemented)
+        //         return
+        //     }
+        //     let arguments = call.arguments as! Dictionary<String, String>
+        //     let url = arguments["url"]
+        //     let data = arguments["data"]
             
-            var jsonData: String?
-            do {
-                print("yeet0")
-                let context = try SwiftyZeroMQ.Context()
-                let socket = try context.socket(.request)
-                print("yeet1")
-                print(url!)
-                print(data!)
-                try socket.connect(url!)
-                print("yeet2")
-                try socket.send(string: data!)
-                print("yeet3")
-                jsonData = try socket.recv()
-                print(jsonData)
-                print("yeet4")
-                try socket.close()
-                try context.terminate()
-                print("yeet5")
-            } catch {
-                print(error)
-            }
+        //     var jsonData: String?
+        //     do {
+        //         let context = try SwiftyZeroMQ.Context()
+        //         let socket = try context.socket(.request)
+        //         print(url!)
+        //         print(data!)
+        //         try socket.connect(url!)
+        //         try socket.send(string: data!)
+        //         jsonData = try socket.recv()
+        //         print(jsonData)
+        //         try socket.close()
+        //         try context.terminate()
+        //     } catch {
+        //         print(error)
+        //     }
             
-            let jsonDataObj = self?.getCurrentWeatherData(jsonData: jsonData!)
-            result(jsonDataObj)
-        })
+        //     let jsonDataObj = self?.getCurrentWeatherData(jsonData: jsonData!)
+        //     result(jsonDataObj)
+        // })
 
         GeneratedPluginRegistrant.register(with: self)
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
     
-    private func getCurrentWeatherData(jsonData: String) -> String {
-        // Get nested JSON object
-        let completeJsonDataObj = try? JSONSerialization.jsonObject(with: jsonData.data(using: .utf8)!, options: []) as? [String : Any]
-        let finalJsonDataObj = (completeJsonDataObj?["nws"] as? [String : Any])?["current"]
-        print(String(describing: finalJsonDataObj))
-        return String(describing: finalJsonDataObj)
-    }
+    // private func getCurrentWeatherData(jsonData: String) -> String {
+    //     // Get nested JSON object
+    //     let completeJsonDataObj = try? JSONSerialization.jsonObject(with: jsonData.data(using: .utf8)!, options: []) as? [String : Any]
+    //     let finalJsonDataObj = (completeJsonDataObj?["nws"] as? [String : Any])?["current"]
+    //     print(String(describing: finalJsonDataObj))
+    //     return String(describing: finalJsonDataObj)
+    // }
 }
