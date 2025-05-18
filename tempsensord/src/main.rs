9c3d0c4a0e300543d8bb73423c0786e1df9a9b50
Brian Hurley <hurleybri924@gmail.com>
2025-05-18 00:38:28 -0400

    Added reminder to fix i2cdev import.

--- tempsensord/src/main.rs
+++ tempsensord/src/main.rs
@@ -1,6 +1,7 @@
 extern crate si7021;
 extern crate i2cdev;
 
+//TODO: Fix error with unresolved import on i2cdev::linux
 use i2cdev::linux::LinuxI2CDevice;
 use si7021::{Si7021, SI7021_I2C_ADDRESS};
 
